84327696f32e0b495a47ee1ccdf878905f36c5c2
abigail9660 <119711919+abigail9660@users.noreply.github.com>
2022-12-03 10:24:33 +0100

    Add files via upload

---
+++ my jupyter notebook.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __Eligbue Abigail__
+# 
+# Pharmacist
+
+# *I am interested in data science because I want to incorporate the knowledge into my existing career*. *Also, I want to venture into various career options in data science*.
+
+# ### List of numbers from a range
+
+# In[4]:
+
+
+range(15)
+
+
+# In[5]:
+
+
+list(range(15))
+
+
+# ***
+# ___
+# -
+# -
+# -
+# ~~jupternotebook~~
+# 
+# # slicing in python
